756153e1e143c369c1e38f8ed5fa6bee3d883a93
Rob Anderson <rob.anderson@ncl.ac.uk>
2019-03-01 09:55:10 +0000

    :star: Add category icons

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 ColoredCard.project-card(:color="categoryColor", interactable)
   router-link.inherit-color(:to="projectRoute")
+    .category-image
+      img(:src="categoryImage", :alt="categoryName")
     h3.title.inherit-color.is-4 {{ project.name }}
     .tags
       .tag.is-white.knockout-text(
@@ -14,6 +16,18 @@ import ColoredCard from './ColoredCard.vue'
 import { ROUTE_PROJECT } from '@/const'
 import CategoryData from '@/data/categories.json'
 
+import AlgorithmsPng from '@/assets/categories/algorithms.png'
+import FairnessPng from '@/assets/categories/fairness.png'
+import MixedPng from '@/assets/categories/mixed.png'
+import SecurityPng from '@/assets/categories/security.png'
+
+const imageMap = {
+  algorithms: AlgorithmsPng,
+  fairness: FairnessPng,
+  mixed: MixedPng,
+  security: SecurityPng
+}
+
 export default {
   components: { ColoredCard },
   props: {
@@ -25,6 +39,13 @@ export default {
     },
     categoryColor() {
       return this.category ? this.category.color : 'blue'
+    },
+    categoryImage() {
+      if (!this.category) return imageMap.mixed
+      return imageMap[this.category.id] || imageMap.mixed
+    },
+    categoryName() {
+      return this.category ? this.category.name : 'Mixed Category'
     },
     projectRoute() {
       const params = { id: this.project.id }
@@ -44,4 +65,17 @@ a
   flex-direction: column
 .title
   flex: 1
+.category-image
+  padding: 1em 2em
++mobile
+  .project-card a
+    flex-direction: row
+  .category-image
+    width: 64px
+    padding: 0
+    margin-right: 0.5em
+  .tags
+    flex: 0
+    flex-direction: column
+    align-items: flex-end
 </style>
